5c9d1a31a513f3a1c966061da3ac66407adfe791
Travis Duffy <travis@travisduffy.dev>
2020-03-05 08:21:07 -0400

    Updated Node script to install version 13

--- scripts/software/node.sh
+++ scripts/software/node.sh
@@ -4,7 +4,7 @@
 sudo add-apt-repository -y -r ppa:chris-lea/node.js
 curl -sSL https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo apt-key add -
 
-VERSION='node_12.x'
+VERSION='node_13.x'
 DISTRO="$(lsb_release -s -c)"
 
 echo "deb https://deb.nodesource.com/$VERSION $DISTRO main" | sudo tee /etc/apt/sources.list.d/nodesource.list
